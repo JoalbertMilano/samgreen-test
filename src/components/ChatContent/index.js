d922f67419a02e5d83c01c564df37b01170229c8
JoalbertMilano <joalbertmilano@gmail.com>
2022-06-05 20:49:14 -0400

    Adding message sent

--- src/components/ChatContent/index.js
+++ src/components/ChatContent/index.js
@@ -3,6 +3,7 @@ import { Search, SentimentSatisfiedAlt } from '@mui/icons-material'
 import { ChatInput } from '../ChatInput'
 import { Nav } from '../Nav'
 import { MessageRecived } from '../MessageRecived'
+import { MessageSent } from '../MessageSent'
 
 export const ChatContent = ({ title, status, avatar }) => {
     return (
@@ -15,11 +16,15 @@ export const ChatContent = ({ title, status, avatar }) => {
                     { status }
                 </Typography>
             </Nav>
-            <Stack flex={1} direction="column" spacing={2} p={5}>
+            <Stack flex={1} direction="column" spacing={2} p={5} sx={{ overflowY: "auto" }}>
                 <MessageRecived 
                     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh malesuada sit."
                     time="9:48 p. m."
                     avatar={avatar}
+                />
+                <MessageSent 
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh malesuada sit."
+                    time="9:48 p. m."
                 />
             </Stack>
             <AppBar position="static">
--- src/components/MessageSent/index.js
+++ src/components/MessageSent/index.js
@@ -1,20 +1,16 @@
-import { Box, Stack, Typography, Avatar, styled } from "@mui/material";
+import { Stack, Typography, Box, styled } from "@mui/material";
 
-export const MessageRecived = ({ avatar, text, time }) => {
+export const MessageSent = ({ text, time }) => {
     const MessageContainer = styled(Stack)(({ theme }) => ({
         padding: "8px",
-        background: theme.palette.primary.light,
+        background: theme.palette.primary.main,
         borderRadius: "12px",
+        color: "white"
     }))
 
     return (
         <Box>
-            <Stack direction="row" alignItems="center" spacing={1} mr={12}>
-                <Avatar sx={{ width: "28px", height: "28px" }}>
-                    <Typography variant='caption' sx={{ color: '#252538', fontWeight: 500 }}>
-                        { avatar }
-                    </Typography>
-                </Avatar>
+            <Stack direction="row" ml={12}>
                 <MessageContainer direction="column" flex={1}>
                     <Typography variant="body1">
                         {text}
